a4ec29f34071c195994b57a0758401cd1b4f226e
Jonatas Melo <jonnmello@hotmail.com>
2021-11-20 19:47:06 -0300

    Update

--- backend/src/main/java/com/devsuperiorcrud/crudclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperiorcrud/crudclient/resources/ClientResource.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,7 +31,7 @@ public class ClientResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	//buscar so pelo id
+	//Buscar
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ClientDTO>findById(@PathVariable Long id){
 		ClientDTO dto = service.findById(id);
@@ -38,7 +39,7 @@ public class ClientResource {
 	}
 	
 	
-	//inserir client
+	//Inserir
 	@PostMapping
 	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
 		dto = service.insert(dto);
@@ -47,6 +48,15 @@ public class ClientResource {
 		return ResponseEntity.created(uri).body(dto);
 				
 	}
+	
+	//Atualizar
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> update (@PathVariable Long id, @RequestBody ClientDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);	
+	}
+	
 	
 	
 }
--- backend/src/main/java/com/devsuperiorcrud/crudclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperiorcrud/crudclient/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperiorcrud.crudclient.dto.ClientDTO;
 import com.devsuperiorcrud.crudclient.entities.Client;
 import com.devsuperiorcrud.crudclient.repositories.ClientRepository;
-import com.devsuperiorcrud.crudclient.services.exceptions.EntityNotFoundException;
+import com.devsuperiorcrud.crudclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -30,7 +32,7 @@ public class ClientService {
 	public ClientDTO findById(Long id) {
 	
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 	    return 	new ClientDTO(entity);
 	}
 
@@ -44,6 +46,27 @@ public class ClientService {
 		entity.setIncome(dto.getIncome());
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+	
+		try {
+		 Client entity = repository.getOne(id);
+		 entity.setName(dto.getName());
+		 entity.setCpf(dto.getCpf());
+		 entity.setChildren(dto.getChildren());
+		 entity.setBirthDate(dto.getBirthDate());
+		 entity.setIncome(dto.getIncome());
+		 entity = repository.save(entity);
+		 return new ClientDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	
+		
 	}
 	
 	
