00ac154cfcbbd7da38268aa059e832ca874c1635
Jonatas Melo <jonnmello@hotmail.com>
2021-11-20 19:22:04 -0300

    Insert

--- backend/src/main/java/com/devsuperiorcrud/crudclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperiorcrud/crudclient/services/ClientService.java
@@ -33,6 +33,18 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 	    return 	new ClientDTO(entity);
 	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity =  new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setChildren(dto.getChildren());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setIncome(dto.getIncome());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
 	
 	
 }
