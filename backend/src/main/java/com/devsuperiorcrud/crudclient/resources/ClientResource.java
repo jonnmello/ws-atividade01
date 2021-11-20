2bbf5a6eb6f46721f188ec46009c7940a303b11a
Jonatas Melo <jonnmello@hotmail.com>
2021-11-20 18:44:05 -0300

    Find By id

--- backend/src/main/java/com/devsuperiorcrud/crudclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperiorcrud/crudclient/resources/ClientResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,10 +19,18 @@ public class ClientResource {
 	@Autowired
 	private ClientService service;
 
+	
 	@GetMapping
 	public ResponseEntity<List<ClientDTO>>findAll(){
 		List<ClientDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	//buscar so pelo id
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO>findById(@PathVariable Long id){
+		ClientDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 	
 }
--- backend/src/main/java/com/devsuperiorcrud/crudclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperiorcrud/crudclient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.devsuperiorcrud.crudclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperiorcrud.crudclient.dto.ClientDTO;
 import com.devsuperiorcrud.crudclient.entities.Client;
 import com.devsuperiorcrud.crudclient.repositories.ClientRepository;
+import com.devsuperiorcrud.crudclient.services.exceptions.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -22,9 +24,15 @@ public class ClientService {
 		List<Client> list = repository.findAll();
 		
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
-		
-		
-}
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+	
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+	    return 	new ClientDTO(entity);
+	}
 	
 	
 }
