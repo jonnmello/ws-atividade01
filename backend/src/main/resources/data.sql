4968d12b3c2199903b71dd92f9a3786c6e910be8
Jonatas Melo <jonnmello@hotmail.com>
2021-11-22 22:19:38 -0300

    Completo

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,15 @@
-INSERT INTO tb_client (name, created_At) VALUES ('JONATAS',NOW());
-INSERT INTO tb_client (name, created_At) VALUES ('JAY',NOW());
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Jonatas Melo', 54794252590, 5000, TIMESTAMP WITH TIME ZONE '1991-04-04T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Jayanne Mendes', 34994992590, 9000, TIMESTAMP WITH TIME ZONE '1996-01-27T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Roberto Valente', 64694002590, 2000, TIMESTAMP WITH TIME ZONE '1992-08-24T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Leia Freitas', 74684912590, 1000, TIMESTAMP WITH TIME ZONE '1999-03-01T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Jeferson Souza', 24699992590, 3000, TIMESTAMP WITH TIME ZONE '2002-02-20T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Jairo Barbosa', 39694454590, 4000, TIMESTAMP WITH TIME ZONE '1980-08-20T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Donny Silva', 84884952580, 5000, TIMESTAMP WITH TIME ZONE '2002-02-09T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Rafael Cordeiro', 94390952590, 7000, TIMESTAMP WITH TIME ZONE '1984-04-30T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Adriana Melo', 05694952994, 8000, TIMESTAMP WITH TIME ZONE '1970-09-17T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Magno Souza', 14624952593, 6000, TIMESTAMP WITH TIME ZONE '2004-08-11T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Paulo Santos', 54694952595, 3000, TIMESTAMP WITH TIME ZONE '2003-02-26T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Roberto Pedra', 94694952512, 2000, TIMESTAMP WITH TIME ZONE '1994-03-05T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Davi Silva', 84694952598, 6000, TIMESTAMP WITH TIME ZONE '1991-01-04T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Geraldo Oliveira', 74694953597, 7000, TIMESTAMP WITH TIME ZONE '1992-09-02T10:30:00Z', 2,NOW())
+INSERT INTO tb_client (name, cpf, income, birth_Date, children, created_At) VALUES ( 'Ben Costa', 04694952590, 8000, TIMESTAMP WITH TIME ZONE '1994-07-20T10:30:00Z', 2,NOW())
